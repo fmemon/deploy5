494818452ea8641968b26b7001925f171e23810a
Your Name <fmemon100@yahoo.com>
2012-02-01 19:20:51 -0500

    TaskD done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,24 @@
 class Product < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true, :length => {:within => 6..20, :too_short => "please enter at least 6 characters"}
+  validates :title, uniqueness: true, :length => {:within => 6..40, :too_short => "please enter at least 6 characters"}
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)$}i, message: "must be a URL for GIF, JPG, or PNG image. "}
+  
+  private
+  
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    
+    end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Depot5::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
