e6d9d944ef48ae4d2853da496801a15b9582661b
Your Name <fmemon100@yahoo.com>
2012-02-01 19:26:59 -0500

    added session[:counter]

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,11 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @cart = current_cart
+    if session[:counter].nil?
+       session[:counter] = 1
+    else
+     session[:counter] += 1
+    end
   end
 
 end
