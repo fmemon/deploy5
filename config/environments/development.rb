b309dc53dc33832ed155de8344b8c36afadd1093
Your Name <fmemon100@yahoo.com>
2012-02-03 12:15:37 -0500

    emailing works; H1 done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,21 @@ Depot5::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+   # Don't actually send emails
+
+  config.action_mailer.delivery_method = :test
+
+  # Alternate configuration example, using gmail:
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #     address:        "smtp.gmail.com",
+  #     port:           587, 
+  #     domain:         "domain.of.sender.net",
+  #     authentication: "plain",
+  #     user_name:      "dave",
+  #     password:       "secret",
+  #     enable_starttls_auto: true
+  #   } 
+
 end
